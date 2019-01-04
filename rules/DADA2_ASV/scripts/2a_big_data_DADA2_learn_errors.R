a143ef0c0e0cb094538d1e2074defc24db1e3046
Valentin <valentin.scherz@chuv.ch>
2019-01-04 18:15:20 +0100

    added error models plots

--- rules/DADA2_ASV/scripts/2a_big_data_DADA2_learn_errors.R
+++ rules/DADA2_ASV/scripts/2a_big_data_DADA2_learn_errors.R
@@ -16,6 +16,8 @@
 # Output
     error_profile_F <- snakemake@output[["error_profile_F"]]
     error_profile_R <- snakemake@output[["error_profile_R"]]
+    error_plot_F <- snakemake@output[["error_profile_F_plot"]]
+    error_plot_R <- snakemake@output[["error_profile_R_plot"]]
 
 # Load needed libraries
     library(dada2); packageVersion("dada2")
@@ -30,3 +32,11 @@
 # Write these error profiles
     saveRDS(object = errF, file = error_profile_F)
     saveRDS(object = errR, file = error_profile_R)
+
+# Write error_plots
+    png(error_plot_F)
+    plotErrors(errF, nominalQ=TRUE)
+    png(error_plot_R)
+    plotErrors(errF, nominalQ=TRUE)
+
+    dev.off()
