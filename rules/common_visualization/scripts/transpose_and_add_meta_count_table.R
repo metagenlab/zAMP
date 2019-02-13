4185fb66f98e4938c3fe68536f774100aca419eb
Valentin <valentin.scherz@chuv.ch>
2019-02-13 09:43:27 +0100

    merged metadata table

---
+++ rules/common_visualization/scripts/transpose_and_add_meta_count_table.R
@@ -0,0 +1,32 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: valentinscherz
+# Created on: 26.10.18
+
+## Inspired from https://gist.github.com/erictleung/eda33bceceebb190eb9a44c93a077d32
+## Redirect R output
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+
+## Input
+count_table <- snakemake@input[["count_table"]]
+meta <- snakemake@input[["meta"]]
+
+## Output
+transposed_table <- snakemake@output[["transposed_table"]]
+merged_meta <- snakemake@output[["merged_meta"]]
+
+## Load the phyloseq phyloseq_object
+table <- read.table(count_table,  sep = "\t", header = TRUE)
+meta <- read.table(meta,  sep = "\t", header = TRUE)
+
+## Tranpose table
+table_t <- t(table)
+
+## Bind tables
+binded_tables <- cbind(meta, table_t)
+
+### Write this table in a tsv file since it is ground for coming analysis and slow to compute
+write.table(x = table_t, file = transposed_table, append = F, quote = F, sep = "\t", eol = "\n", row.names = F, col.names = T )
+write.table(x = binded_tables, file = merged_meta, append = F, quote = F, sep = "\t", eol = "\n", row.names = F, col.names = T )
