de3e2c99998ff9185ba626cdf82668ce2caccf76
Valentin <valentin.scherz@chuv.ch>
2019-06-06 19:52:53 +0200

    working state

--- rules/1_2_DADA2_ASVs/scripts/2d_big_data_DADA2_merge_chimera.R
+++ rules/1_2_DADA2_ASVs/scripts/2d_big_data_DADA2_merge_chimera.R
@@ -1,34 +1,33 @@
-# Title     : DADA2 - merge runs, filter chimera
-# Objective : Merge runs, filter chimera and based on length
+# Title     : DADA2 - merge all and filter
+# Objective : Merge all data from the different reads and filter sequences based on length
 # Created by: valentinscherz
 # Created on: 06.06.19
-# Modified from :https://benjjneb.github.io/dada2/bigdata_paired.html
+# Modified from :https://benjjneb.github.io/dada2/bigdata_paired.html and https://github.com/benjjneb/dada2/issues/704
 
-## Redirect R output
+# Redirect R output
   log <- file(snakemake@log[[1]], open="wt")
   sink(log)
   sink(log, type="message")
 
-## Input
+# Input
   seq_tab <- snakemake@input[["seq_tab"]]
 
 ## Output
   no_chim <- snakemake@output[["no_chim"]]
   length_filtered <- snakemake@output[["length_filtered"]]
-  hashed_sequences <- snakemake@output[["hashed_sequences"]]
-  hashed_count_table <- snakemake@output[["hashed_count_table"]]
   rep_seqs <- snakemake@output[["rep_seqs"]]
   count_table <- snakemake@output[["count_table"]]
   length_histo <- snakemake@output[["length_histo"]]
 
-## Parameters
+# Parameters
   merged_min_length <- snakemake@params[["merged_min_length"]]
   merged_max_length  <- snakemake@params[["merged_max_length"]]
 
-## Load needed libraries
+# Load needed libraries
   library(dada2); packageVersion("dada2")
+  library(ggplot2); packageVersion("ggplot2")
 
-## Merge data from multiple runs (if necessary)
+# Merge data from multiple runs (if necessary)
    if (length(seq_tab) == 1){
 	print("Unique RUN, no merging of seq_tabl")
 	st.all <- readRDS(seq_tab)
@@ -37,50 +36,46 @@
 	st.all <- do.call("mergeSequenceTables", lapply(seq_tab, readRDS))
    }
 
-## Remove chimeras
+# Remove chimeras
   print("Filter chimera")
   seqtab <- removeBimeraDenovo(st.all, method="consensus", multithread=snakemake@threads, verbose=TRUE)
   print("Chimera filtered")
 
-## Sequences length inspection and filtration
-  ### Inspect distribution of sequence lengths
-  seq_length_table <- table(nchar(getSequences(seqtab)))
+# Sequences length inspection and filtration
+#### That's the little added trick, the reason why we are using this script and not the one in Qiime2. Indeed we typically are here keeping only sequences between 390 and 500 bp of length after merging. This tcorresponds to the expected length of the V3V4 region of the 16S rRNA gene.
+  ## Inspect distribution of sequence lengths
+  unfiltered_length_table <- tapply(colSums(seqtab), factor(nchar(getSequences(seqtab))), sum)
+  unfiltered_length_table
+  unfiltered_length_table_df <- data.frame(length=as.numeric(names(unfiltered_length_table)), count=unfiltered_length_table)
   png(length_histo)
-  hist(seq_length_table,
-      main="Unfiltered sequences length",
-      xlab="Length")
+  p <- ggplot(data=unfiltered_length_table_df, aes(x=length, y=count)) + geom_col(binwidth=20)
+  print(p)
   dev.off()
 
-  ## Filter based on length
-    seqtab2 <- seqtab[,nchar(colnames(seqtab)) %in% seq(merged_min_length, merged_max_length)]
-  ## Inspect distribution of sequence lengths after filtration
-    table(nchar(getSequences(seqtab2)))
+# Filter based on length
+seqtab2 <- seqtab[,nchar(colnames(seqtab)) %in% seq(merged_min_length, merged_max_length)]
+# Inspect distribution of sequence lengths after filtration
+table(nchar(getSequences(seqtab2)))
 
-## Export reads and count
-### We are writing in files the product of this DADA2 process. These are one .fasta file contanining the dereplicated, errors corrected, paired-end merged representative sequences and one .txt file indicating the prevalence of sequencne in each sample (this is the result of dereplication).
-  ### Still hashed sequences
-    asv_fasta_hashed <- c(rbind(asv_headers, asv_seqs))
-    write(asv_fasta_hashed, hashed_sequences)
-
-  ### Still hashed count table
-    print("Create hashed count table")
-    asv_tab_hashed <- t(seqtab2)
-    #row.names(asv_tab) <- sub(">", "", asv_headers)
-    write.table(asv_tab_hashed, hashed_count_table , sep="\t", quote=F)
-
-  ### Give our seq headers more manageable names (ASV_1, ASV_2...)
+# Export reads and count
+#### We are writing in files the product of this DADA2 process. These are one .fasta file contanining the dereplicated, errors corrected, paired-end merged representative sequences and one .txt file indicating the prevalence of sequencne in each sample (this is the result of dereplication).
     asv_seqs <- colnames(seqtab2)
+
+    ## giving our seq headers more manageable names (ASV_1, ASV_2...)
     asv_headers <- vector(dim(seqtab2)[2], mode="character")
     for (i in 1:dim(seqtab2)[2]) {
       asv_headers[i] <- paste(">ASV", i, sep="_")
     }
-  ## Make and write out a fasta of our final ASV seqs:
+
+    ## making and writing out a fasta of our final ASV seqs:
     asv_fasta <- c(rbind(asv_headers, asv_seqs))
     write(asv_fasta, rep_seqs)
 
-  ## Count table:
+  ## count table:
     print("Create count table")
     asv_tab <- t(seqtab2)
+
+ ## Renamed
     row.names(asv_tab) <- sub(">", "", asv_headers)
     write.table(asv_tab, count_table , sep="\t", quote=F)
 
---
+++ rules/5_visualization/scripts/create_biom_from_count_table.R
@@ -0,0 +1,26 @@
+# Title     : Create biom from count table
+# Objective : Format count table into biom
+# Created by: valentinscherz
+# Created on: 06.06.19
+
+
+## Redirect R output
+    log <- file(snakemake@log[[1]], open="wt")
+    sink(log)
+    sink(log, type="message")
+
+## Input
+    count_table <- snakemake@input[["count_table"]]
+
+## Output
+    biom_count <- snakemake@output[["biom_count"]]
+
+## Library
+    library(biomformat);packageVersion("biomformat")
+    library(phyloseq);packageVersion("phyloseq")
+
+## Reformat
+    asv_tab<-read.table(file = count_table)
+    otu<-as(otu_table(asv_tab, taxa_are_rows = TRUE),"matrix")
+    otu_biom<-make_biom(data=otu)
+    write_biom(x = otu_biom, biom_file = biom_count)
