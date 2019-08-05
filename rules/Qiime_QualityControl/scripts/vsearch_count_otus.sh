031119c9c910c577c957e6f07dfeaa6a0ff1a1fa
Valentin <valentin.scherz@chuv.ch>
2019-08-05 18:01:33 +0200

    fixed copypaste

--- rules/Qiime_QualityControl/scripts/vsearch_count_otus.sh
+++ rules/Qiime_QualityControl/scripts/vsearch_count_otus.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-_file="$1"
-[ $# -eq 0 ] && { echo "Usage: $0 filename"; exit 1; }
-[ ! -f "$_file" ] && { echo "Error: $0 file not found."; exit 2; }
+_file={input[samples]}
+[ $# -eq 0 ] && {{ echo "Usage: $0 filename"; exit 1; }}
+[ ! -f "$_file" ] && {{ echo "Error: $0 file not found."; exit 2; }}
 
 if [ -s "$_file" ]
 then
-	echo "$_file has some data."
+    echo "$_file has some data." && \
+    vsearch --usearch_global {input[samples]} \
+        -otutabout {output} \
+        -id 1 \
+        -strand plus \
+        --db {input[rep_seq]}
         # do something as file has data
 else
-	echo "$_file is empty."
-        # do something as file is empty
+    echo "$_file is empty." && \
+    touch {input[samples]}
+    # do something as file is empty
 fi
+2> {log}
