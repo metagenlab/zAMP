9768868eda956b6018b3fbe597fed985b7f7e158
Valentin <valentin.scherz@chuv.ch>
2019-08-06 15:05:03 +0200

    WIP copy instead of clone

--- build_n_validate_docker.sh
+++ build_n_validate_docker.sh
@@ -7,6 +7,6 @@ GITHUBAT=$3
 docker build https://$GITHUBAT@github.com/metagenlab/microbiome16S_pipeline.git#$VERSION \
     -t metagenlab/amplicon_pipeline:$VERSION \
     -f ./Dockerfile \
-    --build-arg GITHUB_AT=$_GITHUB_AT 
+    --build-arg GITHUB_AT=$GITHUB_AT
 
 docker build -f ./validation.Docker . --build-arg VERSION=$_VERSION --build-arg GITHUB_AT=$_GITHUB_AT TEST_CPU=$_CPU
