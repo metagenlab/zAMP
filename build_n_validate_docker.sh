c7ab353bce2a99ff2da563b566f8645073c08478
Valentin <valentin.scherz@chuv.ch>
2019-08-06 14:27:54 +0200

    WIP build script

--- build_n_validate_docker.sh
+++ build_n_validate_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-_VERSION=$0
-_CPU=$1
-_GITHUB_AT=$2
+VERSION=$1
+CPU=$2
+GITHUBAT=$3
 
-docker build -f ./Dockerfile --build-arg GITHUB_AT=$_GITHUB_AT --build-arg VERSION=$_VERSION -t $_VERSION
+docker build https://$GITHUBAT@github.com/metagenlab/microbiome16S_pipeline.git#$VERSION -t metagenlab/amplicon_pipeline:$VERSION -f ./Dockerfile . --build-arg GITHUB_AT=$_GITHUB_AT --build-arg VERSION=$_VERSIONN
 
-docker build -f ./validation.Docker --build-arg VERSION=$_VERSION --build-arg GITHUB_AT=$_GITHUB_AT TEST_CPU=$_CPU
+docker build -f ./Dockerfile . --build-arg GITHUB_AT=$_GITHUB_AT --build-arg VERSION=$_VERSION -t $_VERSION
+
+docker build -f ./validation.Docker . --build-arg VERSION=$_VERSION --build-arg GITHUB_AT=$_GITHUB_AT TEST_CPU=$_CPU
