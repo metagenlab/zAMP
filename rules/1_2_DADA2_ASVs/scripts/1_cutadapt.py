12652ea90982edc241e67361451b957b7e247769
Valentin <valentin.scherz@chuv.ch>
2019-09-12 20:26:13 +0200

    working ITS cutadapat script

--- rules/1_2_DADA2_ASVs/scripts/1_cutadapt.py
+++ rules/1_2_DADA2_ASVs/scripts/1_cutadapt.py
@@ -1,36 +1,43 @@
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
+from snakemake import shell
 
-if params.amplicon_type == 'ITS' 
-    forward_primer_compl = Seq.reverse_complement(Seq(params.forward_primer, IUPAC.ambiguous_dna)),
-    reverse_primer_compl = Seq.reverse_complement(Seq(params.reverse_primer, IUPAC.ambiguous_dna))
-    shell("
-        cutadapt \
-        --cores {threads} \
-        --error-rate 0.1 \
-        --times 1 \
-        --overlap 3 \
-        -o {output[R1_trimmed_reads]} \
-        -p {output[R2_trimmed_reads]} \
-        -g '{params[forward_primer]}' -a '{reverse_primer_compl}' \
-        -G '{params[reverse_primer]}' -A '{forward_primer_compl}' \
-        --match-read-wildcards \
-        {input[R1_raw_reads]} \
-        {input[R2_raw_reads]} >> {log}
-    ")
+amplicon = snakemake.params['amplicon_type']
 
-else
-    shell("
-        cutadapt \
-        --cores {threads} \
-        --error-rate 0.1 \
-        --times 1 \
-        --overlap 3 \
-        -o {output[R1_trimmed_reads]} \
-        -p {output[R2_trimmed_reads]} \
-        -g '{params[forward_primer]}' \
-        -G '{params[reverse_primer]}' \
-        --match-read-wildcards \
-        {input[R1_raw_reads]} \
-        {input[R2_raw_reads]} >> {log}
-    ")
+if amplicon == "ITS":
+    print("ITS Trimming")
+    forward_primer_compl = Seq.reverse_complement(Seq(snakemake.params['forward_primer'], IUPAC.ambiguous_dna))
+    reverse_primer_compl = Seq.reverse_complement(Seq(snakemake.params['reverse_primer'], IUPAC.ambiguous_dna))
+    shell("""cutadapt \
+    --cores {snakemake.threads} \
+    --error-rate 0.1 \
+    --times 2 \
+    --overlap 3 \
+    -o {snakemake.output[R1_trimmed_reads]} \
+    -p {snakemake.output[R2_trimmed_reads]} \
+    -g '{snakemake.params[forward_primer]}' \
+    -a '{reverse_primer_compl}' \
+    -G '{snakemake.params[reverse_primer]}' \
+    -A '{forward_primer_compl}' \
+    --match-read-wildcards \
+    {snakemake.input[R1_raw_reads]} \
+    {snakemake.input[R2_raw_reads]} >> {snakemake.log[0]}""")
+
+elif amplicon == "16S":
+    print("16S Trimming")
+    shell("""cutadapt \
+    --cores {snakemake.threads} \
+    --error-rate 0.1 \
+    --times 1 \
+    --overlap 3 \
+    -o {snakemake.output[R1_trimmed_reads]} \
+    -p {snakemake.output[R2_trimmed_reads]} \
+    -g '{snakemake.params[forward_primer]}' \
+    -G '{snakemake.params[reverse_primer]}' \
+    --match-read-wildcards \
+    {snakemake.input[R1_raw_reads]} \
+    {snakemake.input[R2_raw_reads]} >> {snakemake.log[0]}""")
+
+
+
+
