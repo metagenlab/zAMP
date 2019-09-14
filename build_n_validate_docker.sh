91e61157901e2cba040c4b187580a85f3f998cbe
Valentin <valentin.scherz@chuv.ch>
2019-09-14 22:29:06 +0200

    update

--- build_n_validate_docker.sh
+++ build_n_validate_docker.sh
@@ -7,11 +7,14 @@ echo $2
 GITHUBAT=$3
 echo $3
 
-docker build -f ./Dockerfile . \
+docker build https://$GITHUBAT@github.com/metagenlab/microbiome16S_pipeline.git#$VERSION \
+    -t metagenlab/amplicon_pipeline:$VERSION \
+    -f ./Dockerfile \
     --build-arg GITHUB_AT=$GITHUBAT \
     --build-arg TEST_CPU=$CPU
 
-docker build -f ./Dockerfile.validation . \
+docker build https://$GITHUBAT@github.com/metagenlab/microbiome16S_pipeline.git#$VERSION \
+    -f ./Dockerfile.validation \
     --build-arg VERSION=$VERSION \
     --build-arg TEST_CPU=$CPU
 
