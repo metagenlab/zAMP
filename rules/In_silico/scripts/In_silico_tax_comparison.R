d00ae1a4ac9b64c2cc0761e477758ee0776c59c3
Valentin <valentin.scherz@chuv.ch>
2019-10-11 17:59:54 +0200

    Reorder compare table for in silico

--- rules/In_silico/scripts/In_silico_tax_comparison.R
+++ rules/In_silico/scripts/In_silico_tax_comparison.R
@@ -11,26 +11,24 @@ sink(log)
 sink(log, type="message")
 
 
-
-
 ## Load libraries
 library(dplyr);packageVersion("dplyr")
 library(tibble);packageVersion("tibble")
 library(tidyr);packageVersion("tidyr")
 library(phyloseq);packageVersion("phyloseq")
 library(Biostrings);packageVersion("Biostrings")
-
+library(stringi);packageVersion("stringi")
+library(reshape2);packageVersion("reshape2")
 
 ## Input
 count_table <- snakemake@input[["count_table"]]
-
 Metadata_table <- snakemake@input[["Metadata_table"]]
-
 taxonomy_table <- snakemake@input[["taxonomy_table"]]
 
 
 ## Ouput
 output_table <- snakemake@output[["output_table"]]
+output_table_long <- snakemake@output[["output_table_long"]]
 
 ## Parameters
 replace_empty_tax <- snakemake@params[["viz_replace_empty_tax"]]
@@ -48,61 +46,91 @@ metadata <- read.table(file = Metadata_table, sep = "\t", header = TRUE, na.stri
 
 ### Read and format taxonomy table
 print("reading taxonomy table")
-taxonomy_table<-read.table(file = taxonomy_table, header = FALSE, sep = "\t")
+taxonomy_table <- read.table(file = taxonomy_table, header = FALSE, sep = "\t")
+
 
 ### Convert the table into a tabular split version
-taxonomy_table<-taxonomy_table %>% as.tibble() %>% separate(V2, sep=";", c("Kingdom","Phylum","Class","Order","Family","Genus","Species"))
+taxonomy_table_split<-taxonomy_table %>% as_tibble() %>% separate(V2, sep=";", c("Kingdom","Phylum","Class","Order","Family","Genus","Species"))
 
 ### Replace the not properly named headers into proper ones
-colnames(taxonomy_table)[colnames(taxonomy_table)=="V1"] <- "Feature.ID"
-colnames(taxonomy_table)[colnames(taxonomy_table)=="V3"] <- "Confidence"
+colnames(taxonomy_table_split)[colnames(taxonomy_table_split)=="V1"] <- "Feature.ID"
+colnames(taxonomy_table_split)[colnames(taxonomy_table_split)=="V3"] <- "Confidence"
 
-    ### Convert taxonomic levels as character (needed for the next steps)
-    taxonomy_table$Kingdom<-as.character(taxonomy_table$Kingdom)
-    taxonomy_table$Phylum<-as.character(taxonomy_table$Phylum)
-    taxonomy_table$Class<-as.character(taxonomy_table$Class)
-    taxonomy_table$Order<-as.character(taxonomy_table$Order)
-    taxonomy_table$Family<-as.character(taxonomy_table$Family)
-    taxonomy_table$Genus<-as.character(taxonomy_table$Genus)
-    taxonomy_table$Species<-as.character(taxonomy_table$Species)
+### Convert taxonomic levels as character (needed for the next steps)
+taxonomy_table_split$Kingdom<-as.character(taxonomy_table_split$Kingdom)
+taxonomy_table_split$Phylum<-as.character(taxonomy_table_split$Phylum)
+taxonomy_table_split$Class<-as.character(taxonomy_table_split$Class)
+taxonomy_table_split$Order<-as.character(taxonomy_table_split$Order)
+taxonomy_table_split$Family<-as.character(taxonomy_table_split$Family)
+taxonomy_table_split$Genus<-as.character(taxonomy_table_split$Genus)
+taxonomy_table_split$Species<-as.character(taxonomy_table_split$Species)
 
-    print(paste("replace empty taxonomy :", replace_empty_tax))
+print(paste("replace empty taxonomy :", replace_empty_tax))
 
-    if(isTRUE(replace_empty_tax)) {
-      ### Replace NA by the previous order + a space_holder for each taxonomic level
-      taxonomy_table$Kingdom[is.na(taxonomy_table$Kingdom)] <- (("Unkown_Kingdom")[is.na(taxonomy_table$Kingdom)])
-      taxonomy_table$Phylum[is.na(taxonomy_table$Phylum)] <- ((paste(taxonomy_table$Kingdom,"_phy",sep=""))[is.na(taxonomy_table$Phylum)])
-      taxonomy_table$Class[is.na(taxonomy_table$Class)] <- ((paste(taxonomy_table$Phylum,"_clas",sep=""))[is.na(taxonomy_table$Class)])
-      taxonomy_table$Order[is.na(taxonomy_table$Order)] <- ((paste(taxonomy_table$Class,"_ord",sep=""))[is.na(taxonomy_table$Order)])
-      taxonomy_table$Family[is.na(taxonomy_table$Family)] <- ((paste(taxonomy_table$Order,"_fam",sep=""))[is.na(taxonomy_table$Family)])
-      taxonomy_table$Genus[is.na(taxonomy_table$Genus)] <- ((paste(taxonomy_table$Family,"_gen",sep=""))[is.na(taxonomy_table$Genus)])
-      taxonomy_table$Species[is.na(taxonomy_table$Species)] <- ((paste(taxonomy_table$Genus,"_sp",sep=""))[is.na(taxonomy_table$Species)])
+if(isTRUE(replace_empty_tax)) {
+  ### Replace NA by the previous order + a space_holder for each taxonomic level
+  taxonomy_table_split$Kingdom[is.na(taxonomy_table_split$Kingdom)] <- (("Unkown_Kingdom")[is.na(taxonomy_table_split$Kingdom)])
+  taxonomy_table_split$Phylum[is.na(taxonomy_table_split$Phylum)] <- ((paste(taxonomy_table_split$Kingdom,"_phy",sep=""))[is.na(taxonomy_table_split$Phylum)])
+  taxonomy_table_split$Class[is.na(taxonomy_table_split$Class)] <- ((paste(taxonomy_table_split$Phylum,"_clas",sep=""))[is.na(taxonomy_table_split$Class)])
+  taxonomy_table_split$Order[is.na(taxonomy_table_split$Order)] <- ((paste(taxonomy_table_split$Class,"_ord",sep=""))[is.na(taxonomy_table_split$Order)])
+  taxonomy_table_split$Family[is.na(taxonomy_table_split$Family)] <- ((paste(taxonomy_table_split$Order,"_fam",sep=""))[is.na(taxonomy_table_split$Family)])
+  taxonomy_table_split$Genus[is.na(taxonomy_table_split$Genus)] <- ((paste(taxonomy_table_split$Family,"_gen",sep=""))[is.na(taxonomy_table_split$Genus)])
+  taxonomy_table_split$Species[is.na(taxonomy_table_split$Species)] <- ((paste(taxonomy_table_split$Genus,"_sp",sep=""))[is.na(taxonomy_table_split$Species)])
 
-      print("table NA remplaced by spaceholders")
+  print("table NA remplaced by spaceholders")
 
-    }else{
+}else{
 
-      print("table NA NOT remplaced by spaceholders")
-    }
-
-    taxonomy_table$Taxonomy <- unlist(unite(data = taxonomy_table, col = "Taxonomy", c("Kingdom","Phylum","Class","Order","Family","Genus","Species"), sep = ";", remove = TRUE)[2])
-
-comparison <- metadata
-
-for (i in comparison$AssemblyNames){
-  print(i)
-  transposed_counts_i <- transposed_counts[rownames(transposed_counts)==i,]
-  transposed_counts_i_f <- transposed_counts_i[transposed_counts_i > 0]
-
-  for (j in length(names(transposed_counts_i_f))){
-    print(j)
-    comparison[[paste0("Amplicon_", j)]][comparison$AssemblyNames==i] <- names(transposed_counts_i_f)[j]
-    comparison[[paste0("Count_", j)]][comparison$AssemblyNames==i] <- transposed_counts_i_f[j]
-    comparison[[paste0("Tax_", j)]][comparison$AssemblyNames==i] <- taxonomy_table$Taxonomy[taxonomy_table$Feature.ID == names(transposed_counts_i_f)[j]]
-
-  }
+  print("table NA NOT remplaced by spaceholders")
 }
 
+## Re-unit the taxonomy after inclusion of spaceholders
+taxonomy_table_split$Taxonomy <- unlist(unite(data = taxonomy_table_split, col = "Taxonomy", c("Kingdom","Phylum","Class","Order","Family","Genus","Species"), sep = ";", remove = TRUE)[2])
+
+## Create a metadata table to work with
+comparison <- metadata
 
 
-write.table(x = comparison, file = output_table, sep="\t", quote=F, na = "0")
+## Loop over the Assemblies
+for (i in comparison$AssemblyNames){
+  print(i)
+  ### Keep the row of the assembly
+  transposed_counts_i <- transposed_counts[rownames(transposed_counts)==i,]
+  ### Keep ony variants with non-null counts
+  transposed_counts_i_f <- transposed_counts_i[transposed_counts_i > 0]
+  ### Count the number of variants
+  comparison[["Number of variants"]][comparison$AssemblyNames==i] <- length(names(transposed_counts_i_f))
+
+  for (j in 1:length(names(transposed_counts_i_f))){
+    print(j)
+    ## Recover Create columns for each variants, count it, and give the corresponding taxonomy
+    comparison[[paste0("Amplicon_", j)]][comparison$AssemblyNames==i] <- names(transposed_counts_i_f)[j]
+    comparison[[paste0("Count_", j)]][comparison$AssemblyNames==i] <- transposed_counts_i_f[j]
+    comparison[[paste0("Tax_", j)]][comparison$AssemblyNames==i] <- taxonomy_table_split$Taxonomy[taxonomy_table_split$Feature.ID == names(transposed_counts_i_f)[j]]
+    j <- NULL
+  }
+  i <- NULL
+}
+
+## Add the cumulated some for all variants
+sum_of_count <- data.frame(rowSums(t(count_table)))
+colnames(sum_of_count)[colnames(sum_of_count)=="rowSums.t.count_table.."] <- "Sum of copies"
+sum_of_count$AssemblyNames <- rownames(sum_of_count)
+comparison <- left_join(comparison, sum_of_count)
+
+
+write.table(x = comparison, file = output_table, sep="\t", quote=F, row.names = FALSE)
+
+## Create a table with the same informations but in a long format
+## Create an OTU column
+count_table$Feature.ID <- rownames(count_table)
+## Match the position between the count and tax table and joined them
+m <- match(count_table$Feature.ID, taxonomy_table$V1)
+count_table_tax <- cbind(count_table, taxonomy_table$V2[m])
+colnames(count_table_tax)[colnames(count_table_tax)=="taxonomy_table$V2[m]"] <- "Class_tax"
+
+## Melt it in long
+melt_table <- melt(count_table_tax, id.vars = c("Feature.ID", "Class_tax"), variable.name = "AssemblyNames") %>% filter(value>0)
+
+compare_long <- right_join(metadata,melt_table)
+write.table(x = compare_long, file = output_table_long, sep="\t",quote=F, row.names = FALSE)
