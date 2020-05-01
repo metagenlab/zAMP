476c10ecd996be9d8e4dfffc0e4f0399d15cd12f
Valentin Scherz <33422164+valscherz@users.noreply.github.com>
2020-05-01 16:57:54 +0200

    Discard reads without primers
    Fix for https://github.com/metagenlab/microbiome16S_pipeline/issues/22

--- rules/1_2_DADA2_ASVs/scripts/1_cutadapt.py
+++ rules/1_2_DADA2_ASVs/scripts/1_cutadapt.py
@@ -18,6 +18,7 @@ if snakemake.params['amplicon_type'] == "ITS":
     -G '{snakemake.params[reverse_primer]}' \
     -A '{forward_primer_compl}' \
     --match-read-wildcards \
+    --discard-untrimmed \
     {snakemake.input[R1_raw_reads]} \
     {snakemake.input[R2_raw_reads]} >> {snakemake.log[0]}""")
 
@@ -33,6 +34,7 @@ elif snakemake.params['amplicon_type'] == "16S":
     -g '{snakemake.params[forward_primer]}' \
     -G '{snakemake.params[reverse_primer]}' \
     --match-read-wildcards \
+    --discard-untrimmed \
     {snakemake.input[R1_raw_reads]} \
     {snakemake.input[R2_raw_reads]} >> {snakemake.log[0]}""")
 
