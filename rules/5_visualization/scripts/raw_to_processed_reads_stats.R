6dcb9be23b62274712008dc0de24e1cd7387b26c
Valentin <valentin.scherz@chuv.ch>
2024-04-12 09:33:21 +0200

    fix read stats in single reads

--- rules/5_visualization/scripts/raw_to_processed_reads_stats.R
+++ rules/5_visualization/scripts/raw_to_processed_reads_stats.R
@@ -11,8 +11,7 @@
 ## Input
     phyloseq_object <- snakemake@input[["phyloseq_object"]]
     multi_QC_report_path <- snakemake@input[["multi_QC_report_path"]]
-    multi_QC_report <- read.table(multi_QC_report_path, header = T)
-
+    
 ## Ouput
     raw_to_filtered_reads_stats <- snakemake@output[["raw_to_filtered_reads_stats"]]
 
@@ -21,19 +20,22 @@
     library("data.table");packageVersion("data.table")
     library("dplyr");packageVersion("dplyr")
 
+## Load MultiQC report
+    multi_QC_report <- read.table(multi_QC_report_path, header = T)
+
 ## Load the phyloseq object
     phyloseq_obj <- readRDS(phyloseq_object)
 
 ## Create a table with the number of raw reads and filtered reads
-
 ### Filtered reads
     reads_counts_df <- data.table(as(sample_data(phyloseq_obj), "data.frame"), TotalReads = sample_sums(phyloseq_obj), keep.rownames = TRUE)
     setnames(reads_counts_df, "rn", "Sample") # Rename the first column of this news dataframe -> Sample
 
 ### Raw reads
-    multi_QC_report <- multi_QC_report %>% filter(grepl("R1", Sample)) %>% select(c("Sample","FastQC_mqc.generalstats.fastqc.total_sequences")) # keep only the total of raw sequences. Since it is already twice (R1, R2), keep only R1.
+    multi_QC_report <- multi_QC_report %>% filter(grepl("R1|single", Sample)) %>% select(c("Sample","FastQC_mqc.generalstats.fastqc.total_sequences")) # keep only the total of raw sequences. Since it is already twice (R1, R2), keep only R1.
     multi_QC_report$Sample <- gsub(x=multi_QC_report$Sample, pattern = "_R1", replacement = "") # Remove the "_R1"
-
+    #or
+    multi_QC_report$Sample <- gsub(x=multi_QC_report$Sample, pattern = "_single", replacement = "") # Remove the "_single"
 ### Raw reads Join the two dataframes, the one with the filtered reads from phyloseq and the one with the raw reads.
     reads_counts_df_with_raw <- merge(multi_QC_report, reads_counts_df, by=c("Sample"))
 
