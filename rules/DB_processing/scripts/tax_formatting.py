35b648caf2d0fafeac743524c5d4679ac54905fd
farchaab <farid.chaabane@chuv.ch>
2024-06-13 12:41:02 +0200

    Added NaN propagation to downstream ranks, fixed unsorted unique taxa

--- rules/DB_processing/scripts/tax_formatting.py
+++ rules/DB_processing/scripts/tax_formatting.py
@@ -3,8 +3,25 @@ import numpy as np
 
 
 # Functions
+def propagate_nan(df):
+    # Identify columns that have NaN values
+    nan_mask = df.isna()
+
+    # Create a cumulative sum mask to propagate NaNs
+    nan_cumsum = nan_mask.cumsum(axis=1)
+
+    # Any value where the cumulative sum is greater than 0 should be NaN
+    df[nan_cumsum > 0] = np.nan
+
+    return df
+
+
 def dedup_set(s):
     return list(set(s))[0]
+
+
+def sorted_set(s):
+    return sorted(set(s))
 
 
 def format_discrepant_tax(rank, tax, rank_lim=None, print_desc=True):
@@ -33,10 +50,7 @@ def problematic_taxa(row):
 
 # Ranks list
 ranks = ["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
-ranks_lim = {
-    "Species": snakemake.params.numbers_species,
-    "Genus": snakemake.params.numbers_genus,
-}
+ranks_lim = snakemake.params.tax_collapse
 
 # Read tables
 tax_df = pd.read_csv(snakemake.input.tax, sep="\t", names=["seq_id", "tax"])
@@ -60,13 +74,26 @@ uc_df = pd.read_csv(
 ## Split taxonomy path into one column per rank
 lintax_df = tax_df.tax.str.split(";", expand=True).loc[:, 0:6]
 lintax_df.columns = ranks
+
 ## Replace empty values by NaN
 lintax_df = lintax_df.replace("", np.nan).fillna(np.nan)
 
-## Replace NaN values by latest no NaN value (forward fill)
-filled_df = lintax_df.ffill(axis=1)
+## Replace spaces with underscores (some genera names have spaces)
+lintax_df.replace(to_replace=r" ", value="-", regex=True, inplace=True)
 
-## Propagate latest non NaN taxon to empty ones
+## Replace taxa containing and Unkown or Incertae with NaN
+lintax_df.replace(
+    to_replace=r".*Incertae.*|.*Unknown.*", value=np.nan, regex=True, inplace=True
+)
+
+## Replace endosymbionts by NaN
+lintax_df.replace("endosymbionts", np.nan, inplace=True)
+
+
+## Propagate NaN value to downstream ranks and fill NaN with latest non NaN value
+filled_df = propagate_nan(lintax_df).ffill(axis=1)
+
+
 ## Check repeated values and append them with first letter rank suffix
 prop_tax_df = pd.DataFrame()
 for n, rank in enumerate(ranks):
@@ -79,6 +106,7 @@ for n, rank in enumerate(ranks):
         prop_tax_df[f"{rank}"] = lintax_df[f"{rank}"].combine_first(duplicated)
     else:
         prop_tax_df[f"{rank}"] = filled_df[f"{rank}"]
+
 
 ## Get classified species index
 index = ~prop_tax_df["Species"].str.contains("_s")
@@ -130,7 +158,7 @@ multi_df = clust_df[clust_df.seq_counts > 1]
 
 # Deal with discrepant clusters (contain multiple taxonomies)
 ## List unique taxa by cluster for all ranks
-multi_df = multi_df.groupby("clust_id", as_index=False)[ranks].aggregate(set)
+multi_df = multi_df.groupby("clust_id", as_index=False)[ranks].aggregate(sorted_set)
 
 
 # Flag clusters with multiple taxa as descrepent for all ranks
