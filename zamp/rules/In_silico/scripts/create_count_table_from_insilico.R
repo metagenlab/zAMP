662ee9386ffae3dd6088756fd8482056a1bb12eb
farchaab <farid.chaabane@chuv.ch>
2024-09-23 23:06:53 +0200

    re-fixed count tables

--- zamp/rules/In_silico/scripts/create_count_table_from_insilico.R
+++ zamp/rules/In_silico/scripts/create_count_table_from_insilico.R
@@ -18,39 +18,24 @@
 ## Output
     count_table <- snakemake@output[["count_table"]]
 
-## Reformat
-    otus_table<-as.data.frame(array(dim=c(1,3)))
-    colnames(otus_table) <- c("sample", "V1", "V2")
-
-
-## Reformat
-    otus_table<-as.data.frame(array(dim=c(1,3)))
-    colnames(otus_table) <- c("sample", "V1", "V2")
-
+otus_table <- data.frame()
 
     for (xx in count_table_samples){
-    print(paste("Processing", xx))
-    sample <- gsub("_count_table.tsv", "", basename(xx))
-    print(paste("sample:", sample))
-    table <- as.data.frame(array(dim=c(1,2)))
-    table$V1 <- "No_amp"
-    table$V2 <- 0
-    try(table <-read.table(file = xx, sep="\t", as.is=T, header=F))
-    table <- cbind("sample"=sample, table)
-    print(table)
-    otus_table <- rbind(otus_table, table)
+        print(paste("Processing", xx))
+        sample <- gsub("_count_table.tsv", "", basename(xx))
+        print(paste("Sample:", sample))
+        if (file.size(xx) == 0){
+            table <- cbind(Sample=sample, Seq_ID='No_amp', counts=1)
+        } else {
+            try(table <-read.table(file = xx, sep="\t", as.is=T, header=F))
+            table <- cbind(Sample=sample, Seq_ID=table[,1] , counts=rowSums(table[,2:ncol(table), drop=FALSE]))
+        }
+        otus_table <- rbind(otus_table, table)
     }
 
-    otus_table <- otus_table[-1,]
-    colnames(otus_table) <- c("sample", "Seq_ID", "counts")
-
-
-## Transform this table to have a wide format where we have a column by sample
-    transf_vsearch_table <- otus_table %>%
-      group_by(sample, Seq_ID) %>%
-      summarise(counts = sum(counts)) %>%
-      dcast(Seq_ID ~ Sample)
-
+otus_table$counts <- as.numeric(as.character(otus_table$counts))
+## Transform this table to have a wide format where we have samples as columns
+transf_vsearch_table <- spread(otus_table, Sample, counts, fill=0)
 ## Set OTU as rownames
 rownames(transf_vsearch_table) <- transf_vsearch_table$Seq_ID
 transf_vsearch_table <- transf_vsearch_table[,-1]
