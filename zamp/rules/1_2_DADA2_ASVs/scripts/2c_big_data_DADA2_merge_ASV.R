76583ee09b8e64df3b0c9e36f768edac13081af5
Trestan Pillonel <trestan.pillonel@gmail.com>
2025-03-20 14:55:09 +0100

    Raise an error if all samples of the run produced 0 ASV (Should not happen...)

--- zamp/rules/1_2_DADA2_ASVs/scripts/2c_big_data_DADA2_merge_ASV.R
+++ zamp/rules/1_2_DADA2_ASVs/scripts/2c_big_data_DADA2_merge_ASV.R
@@ -20,12 +20,20 @@
 ## Load needed libraries
     library(dada2); packageVersion("dada2")
 
+    sample_seq_table_no_empty <- sample_seq_table[file.size(sample_seq_table) != 0]
+    infer_stats_no_empty <- infer_stats[file.size(infer_stats) != 0]
+    print("Number of non empty files")
+    print(length(sample_seq_table_no_empty))
+
 ## Merge and write paired-end merge reads of the run
-    input_M <- sapply(sample_seq_table, readRDS, simplify = TRUE, USE.NAMES = FALSE)
+    print("Merge and write paired-end merge reads of the run")
+    input_M <- sapply(sample_seq_table_no_empty, readRDS, simplify = TRUE, USE.NAMES = FALSE)
     st.all <- makeSequenceTable(input_M)
+    if (ncol(st.all) == 0) {stop(paste("Zero sequences could be merged for the entire run: ", paste(sample_seq_table, sep=", ")))}
     saveRDS(object = st.all, file = run_seq_table)
 
 ## Merge and write forward reads stats of the run
-    stats <- do.call("rbind", lapply(infer_stats, readRDS))
+    print("Merge and write forward reads stats of the run")
+    stats <- do.call("rbind", lapply(infer_stats_no_empty, readRDS))
     saveRDS(object = stats, file = run_stats)
 
