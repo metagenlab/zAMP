cc67b1719c7a2888ce9561c7621ef81ba8541333
farchaab <farid.chaabane@chuv.ch>
2025-04-08 12:30:07 +0200

    update handling ranks index

--- zamp/rules/3_tax_assignment/scripts/format_RDP_output.py
+++ zamp/rules/3_tax_assignment/scripts/format_RDP_output.py
@@ -8,9 +8,12 @@ if len(sys.argv) != 4:
 
 ranks = sys.argv[1].split(",")
 df = pd.read_csv(sys.argv[2], sep="\t", header=None)
-out_df = df[[0, 5, 8, 11, 14, 17, 20, 23, 25]]
-out_df.columns = ["seq_id"] + ranks + ["confidence"]
-out_df.insert(1, "tax", out_df[ranks].T.agg(";".join))
-out_df[["seq_id", "tax", "confidence"]].to_csv(
-    sys.argv[2], sep="\t", index=False, header=False
+ranks_idx = list(range(5, (len(ranks) - 1) * 4, 3))
+conf_idx = ranks_idx[-1] + 2
+cols = [0] + ranks_idx + [conf_idx]
+df = df[cols]
+df.columns = ["seq_id"] + ranks + ["confidence"]
+df.insert(1, "tax", df[ranks].T.agg(";".join))
+df[["seq_id", "tax", "confidence"]].to_csv(
+    sys.argv[3], sep="\t", index=False, header=False
 )
